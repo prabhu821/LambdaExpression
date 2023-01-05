93c8b8f42cc04d8c76a2ada367130f6512e876f6
rishiraj007 <rishiraj@mounttalent.com>
2023-01-05 10:09:35 +0530

    [Prabhu] Add: Validate Last Name

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -10,7 +10,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nEnter option to choose \n1.First Name \n2.Exit");
+                Console.WriteLine("\nEnter option to choose \n1.First Name \n2.Last Name \n3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -18,6 +18,11 @@
                         Console.WriteLine("\nEnter First Name");
                         string fName = Console.ReadLine();
                         Console.WriteLine(registration.ValidateFirstName(fName));
+                        break;
+                    case 2:
+                        Console.WriteLine("\nEnter Last Name");
+                        string lName = Console.ReadLine();
+                        Console.WriteLine(registration.ValidateLastName(lName));
                         break;
                     default:
                         flag = false;
--- UserRegistrationProblem/UserRegistration.cs
+++ UserRegistrationProblem/UserRegistration.cs
@@ -10,7 +10,10 @@ namespace UserRegistrationProblem
     internal class UserRegistration
     {
         public const string FIRSTNAME = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public const string LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+
         public string ValidateFirstName(string fName) => Regex.IsMatch(fName, FIRSTNAME) ? "Match" : "No Match";
+        public string ValidateLastName(string lName) => Regex.IsMatch(lName, LASTNAME) ? "Match" : "No Match";
 
     }
 }
