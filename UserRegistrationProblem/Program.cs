10c7c3101412910da1a8c1c8389288c83cbd2004
rishiraj007 <rishiraj@mounttalent.com>
2023-01-05 10:06:09 +0530

    [Prabhu] Add: Validate First Name

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem");
+
+            UserRegistration registration = new UserRegistration();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter option to choose \n1.First Name \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\nEnter First Name");
+                        string fName = Console.ReadLine();
+                        Console.WriteLine(registration.ValidateFirstName(fName));
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ UserRegistrationProblem/UserRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UserRegistration
+    {
+        public const string FIRSTNAME = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public string ValidateFirstName(string fName) => Regex.IsMatch(fName, FIRSTNAME) ? "Match" : "No Match";
+
+    }
+}
