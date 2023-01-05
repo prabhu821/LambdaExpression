f99f3602ba49d935e4d897a0839957cf906bb18f
rishiraj007 <rishiraj@mounttalent.com>
2023-01-05 10:04:00 +0530

    [Prabhu] Add: User Registration Problem

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem");
+        }
+    }
+}
